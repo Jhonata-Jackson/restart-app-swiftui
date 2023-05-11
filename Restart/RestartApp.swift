5454cb05b190025395bfaab9fcfdc14fb869ae1f
Jhonata Jackson <jhonata.monteiro@calindra.com.br>
2023-05-11 09:09:41 -0300

    feat: :sparkles: upload restart project to github

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Jhonata Jackson on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Jhonata Jackson on 10/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    var body: some View {
+        if isOnboardingViewActive {
+            OnbordingView()
+        } else {
+            HomeView()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
